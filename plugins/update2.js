f6fd5a53ce14fb27760d54957201cc648513568b
ParadNotFound <98020390+ParadNotFound@users.noreply.github.com>
2022-01-22 18:03:41 +0800

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'unx21/ayame'
+let repository = 'ParadNotFound'
 let branch = 'zx'
 
 async function handler(m, { text }) {
