223d9bd78ffb50b40903889817fb1e70512f2a90
ParadNotFound <98020390+ParadNotFound@users.noreply.github.com>
2022-01-22 16:57:14 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '62887436568613', 'ブラック', m)
+  this.sendContact(m.chat, '62895704217116', 'ParadNotFound, m)
 }
 //handler.help = ['owner', 'creator']
 //handler.tags = ['info']
