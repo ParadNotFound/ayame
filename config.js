30852ab92fd0d7a0cc270daef133a80c5c98fe79
ParadNotFound <98020390+ParadNotFound@users.noreply.github.com>
2022-01-30 10:50:15 +0800

    Create config.js

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ global.APIKeys = JSON.parse(fs.readFileSync('./src/apikey.json'))
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '百鬼あやめ'
-  var sticker_author = 'Kokoronationz × Unx'
+  var sticker_name = 'BYLAZERBOTz'
+  var sticker_author = '~Parad'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
